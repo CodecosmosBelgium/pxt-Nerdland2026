80c1a4b2186c67fa55774da561748820dba2280e
JasperRutten1 <60922940+JasperRutten1@users.noreply.github.com>
2026-04-07 14:39:42 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -11,36 +11,32 @@ namespace CodeFever {
         Rechts
     }
 
-    function blockMetSpoor(powered: boolean) {
-        let list = [REDSTONE_BLOCK, POWERED_RAIL, AIR]
-        if (!(powered)) {
-            list[0] = RED_CONCRETE
-            list[1] = RAIL
+    //% block="Plaats Koper"
+    export function placeCopper(){
+        let position = agent.getPosition().add(pos(0, -1, 0));
+        if(blocks.testForBlock(LIGHT_GRAY_CONCRETE, position)) player.say("Kan geen koper plaatsen op deze plaats");
+        else {
+            blocks.place(599, position)
         }
-        for (let index = 0; index <= list.length - 1; index++) {
-            builder.place(list[index])
-            builder.move(UP, 1)
-        }
-        builder.shift(1, -3, 0)
     }
 
-    //% block="Beweeg Agent"
-    //% richting.defl=vooruit
-    export function moveAgent(richting: Richting) {
-        switch(richting){
-            case Richting.Vooruit:
-                agent.move(FORWARD, 1);
-                break;
-            case Richting.Achteruit:
-                agent.move(BACK, 1);
-                break;
-            case Richting.Links:
-                agent.move(LEFT, 1);
-                break;
-            case Richting.Rechts:
-                agent.move(RIGHT, 1);
-                break;
-        }
-        
+    //% block="Beweeg Vooruit"
+    export function moveAgentForward() {
+        agent.move(FORWARD, 1);
+    }
+
+    //% block="Beweeg Vooruit"
+    export function moveAgentBack() {
+        agent.move(BACK, 1);
+    }
+
+    //% block="Beweeg Vooruit"
+    export function moveAgentLeft() {
+        agent.move(LEFT, 1);
+    }
+
+    //% block="Beweeg Vooruit"
+    export function moveAgentRight() {
+        agent.move(RIGHT, 1);
     }
 }
